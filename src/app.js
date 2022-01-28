efb0a4bd1f323cbbe2238cd33073de5c786c1b1a
niclotta <nicolas.clotta@gmail.com>
2022-01-28 17:00:41 -0300

    undo commit 43f09a1

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const path = require('path');
-const router = require('./routes/router');
-const controller = require('./controller/controller');
+const router = require('./router');
+const controller = require('./controller');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.set('view engine', 'ejs');
-//app.set('views', '../views');
+app.set('views', '../views');
 
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000,() => console.log('El servidor funciona'));
-
-
