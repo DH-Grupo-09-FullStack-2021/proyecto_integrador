44f348b1223e789acda7236453d21a3e053b8825
niclotta <nicolas.clotta@gmail.com>
2022-04-21 18:43:37 -0300

    untabify

--- src/controllers/controllerProductos.js
+++ src/controllers/controllerProductos.js
@@ -5,163 +5,163 @@ const controllerProductos =
 {
     product: (req, res) =>
     {
-	(async () => {
-	    let producto = await db.product.findOne({where: {id: req.params.id}});
-	    if (producto === null)
-		return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
+        (async () => {
+            let producto = await db.product.findOne({where: {id: req.params.id}});
+            if (producto === null)
+                return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
 
-	    let prodlist = await db.product.findAll({where: {id: {[db.Op.ne]: req.params.id}}, order: db.Sequelize.literal('rand()'), limit: 4});
-	    return res.render("product", {product: producto, productos_lista: prodlist});
-	})();
+            let prodlist = await db.product.findAll({where: {id: {[db.Op.ne]: req.params.id}}, order: db.Sequelize.literal('rand()'), limit: 4});
+            return res.render("product", {product: producto, productos_lista: prodlist});
+        })();
     },
     
     submit: (req, res) =>
     {
-	res.render("submit");
+        res.render("submit");
     },
     
     submitPOST: (req, res) =>
-    {	
-	(async () => {
-	    let errors = {};
+    {   
+        (async () => {
+            let errors = {};
 
-	    if (req.body.nombreproducto.trim() == "")
-		errors.nombreproducto = "Debe ingresar el nombre del producto";
+            if (req.body.nombreproducto.trim() == "")
+                errors.nombreproducto = "Debe ingresar el nombre del producto";
 
-	    if (req.body.valorproducto == 0)
-		errors.valorproducto = "Debe ingresar el valor comercial del producto";
+            if (req.body.valorproducto == 0)
+                errors.valorproducto = "Debe ingresar el valor comercial del producto";
 
-	    if (req.body.fabricadorproducto.trim() == "")
-		errors.fabricadorproducto = "Debe ingresar el nombre del fabricante del producto";
+            if (req.body.fabricadorproducto.trim() == "")
+                errors.fabricadorproducto = "Debe ingresar el nombre del fabricante del producto";
 
-	    if (!req.file.mimetype.match("image.*"))
-		errors.imagenprod = "El archivo correspondiente a la imagen del producto debe ser una imagen";
+            if (!req.file.mimetype.match("image.*"))
+                errors.imagenprod = "El archivo correspondiente a la imagen del producto debe ser una imagen";
 
-	    if (req.file.filename == "")
-		errors.imagenprod = "Debe subir una imagen del producto";
+            if (req.file.filename == "")
+                errors.imagenprod = "Debe subir una imagen del producto";
 
-	    if (req.body.descprod.trim() == "")
-		errors.descprod = "Debe ingresar una descripción del producto";
+            if (req.body.descprod.trim() == "")
+                errors.descprod = "Debe ingresar una descripción del producto";
 
-	    if (Object.keys(errors).length == 0)
-	    {
-		let p = await db.product.findOne({where: {name: req.body.nombreproducto}});
-		if (p === null)
-		{
-		    const newprod = db.product.build({
-			name: req.body.nombreproducto.trim(),
-			price: req.body.valorproducto,
-			maker: req.body.fabricadorproducto.trim(),
-			desc: req.body.descprod.trim(),
-			img: req.file.filename
-		    });
-		    
-		    newprod.save();
-		    
-		    return res.redirect("/products");
-		}
-		else
-		    errors.nombreproducto = "Ya existe un producto con este nombre en la base de datos";
-	    }
+            if (Object.keys(errors).length == 0)
+            {
+                let p = await db.product.findOne({where: {name: req.body.nombreproducto}});
+                if (p === null)
+                {
+                    const newprod = db.product.build({
+                        name: req.body.nombreproducto.trim(),
+                        price: req.body.valorproducto,
+                        maker: req.body.fabricadorproducto.trim(),
+                        desc: req.body.descprod.trim(),
+                        img: req.file.filename
+                    });
+                    
+                    newprod.save();
+                    
+                    return res.redirect("/products");
+                }
+                else
+                    errors.nombreproducto = "Ya existe un producto con este nombre en la base de datos";
+            }
 
-	    return res.render("submit", {errors: errors, old: req.body});
-	})();
+            return res.render("submit", {errors: errors, old: req.body});
+        })();
     },
     
     editar: (req, res) =>
     {
-	(async () => {
-	    let producto = await db.product.findOne({where: {id: req.params.id}});
-	    
-	    if (producto === null)
-		return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
-	    else
-		return res.render("editar", {producto: producto});
-	})();
+        (async () => {
+            let producto = await db.product.findOne({where: {id: req.params.id}});
+            
+            if (producto === null)
+                return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
+            else
+                return res.render("editar", {producto: producto});
+        })();
     },
 
     editarPUT: (req, res) =>
-    {	
-	(async () => {
-	    let errors = {};
+    {   
+        (async () => {
+            let errors = {};
 
-	    if (req.body.nombreproducto.trim() == "")
-		errors.nombreproducto = "Debe ingresar el nombre del producto";
+            if (req.body.nombreproducto.trim() == "")
+                errors.nombreproducto = "Debe ingresar el nombre del producto";
 
-	    if (req.body.valorproducto == 0)
-		errors.valorproducto = "Debe ingresar el valor comercial del producto";
+            if (req.body.valorproducto == 0)
+                errors.valorproducto = "Debe ingresar el valor comercial del producto";
 
-	    if (req.body.fabricdorproducto.trim() == "")
-		errors.fabricadorproducto = "Debe ingresar el nombre del fabricante del producto";
+            if (req.body.fabricdorproducto.trim() == "")
+                errors.fabricadorproducto = "Debe ingresar el nombre del fabricante del producto";
 
-	    if (req.body.descprod.trim() == "")
-		errors.descprod = "Debe ingresar una descripción del producto";
-	    
-	    if (Object.keys(errors).length == 0)
-	    {
-		let p = await db.product.findOne({where: {id: req.params.id}});
-		if (p === null)
-		    return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
+            if (req.body.descprod.trim() == "")
+                errors.descprod = "Debe ingresar una descripción del producto";
+            
+            if (Object.keys(errors).length == 0)
+            {
+                let p = await db.product.findOne({where: {id: req.params.id}});
+                if (p === null)
+                    return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
 
-		p.name = req.body.nombreproducto.trim();
-		p.price = req.body.valorproducto;
-		p.maker = req.body.fabricadorproducto.trim();
-		p.desc = req.body.descprod.trim();
-		p.save();
+                p.name = req.body.nombreproducto.trim();
+                p.price = req.body.valorproducto;
+                p.maker = req.body.fabricadorproducto.trim();
+                p.desc = req.body.descprod.trim();
+                p.save();
 
-		return res.redirect("/products");
-	    }
+                return res.redirect("/products");
+            }
 
-	    return res.render("editar", {errors: errors, old: req.body});
-	})();
+            return res.render("editar", {errors: errors, old: req.body});
+        })();
     },
     
     plist: (req, res) =>
     {
-	(async () => {
-	    let products = await db.product.findAll({limit: 8});
-	    return res.render("plist", {productos_lista: products});
-	})();
+        (async () => {
+            let products = await db.product.findAll({limit: 8});
+            return res.render("plist", {productos_lista: products});
+        })();
     },
 
     destroy: (req, res) =>
     {
-	(async () => {
-	    let p = await db.product.findByPk(req.params.id);
-	    if (p === null)
-		return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
+        (async () => {
+            let p = await db.product.findByPk(req.params.id);
+            if (p === null)
+                return res.render('not-found', {errno: 404, errmsg: "El indice no corresponde a ningun producto"});
 
-	    p.destroy();
+            p.destroy();
 
-	    return res.redirect('/products');
-	})();
+            return res.redirect('/products');
+        })();
     },
 
     api: (req, res) =>
     {
-	(async () => {
-	    let productslist = [];
-	    const {count, rows} = await db.product.findAndCountAll();
-	    rows.forEach(product =>
-			 {
-			     productslist.push(product.toJSON());
-			 });
-	    res.send({count: count, products: productslist});
-	})();
+        (async () => {
+            let productslist = [];
+            const {count, rows} = await db.product.findAndCountAll();
+            rows.forEach(product =>
+                         {
+                             productslist.push(product.toJSON());
+                         });
+            res.send({count: count, products: productslist});
+        })();
     },
 
     apiID: (req, res) =>
     {
-	(async () => {
-	    let p = await db.product.findByPk(req.params.id);
-	    let c = await db.compra.findAll({where: {productId: req.params.id}});
-	    let compras = [];
-	    c.forEach(compra =>
-		      {
-			  compras.push(compra.toJSON());
-		      });
-	    res.send({product: p.toJSON(), compras: compras});
-	})();
+        (async () => {
+            let p = await db.product.findByPk(req.params.id);
+            let c = await db.compra.findAll({where: {productId: req.params.id}});
+            let compras = [];
+            c.forEach(compra =>
+                      {
+                          compras.push(compra.toJSON());
+                      });
+            res.send({product: p.toJSON(), compras: compras});
+        })();
     }
 };
 
--- src/controllers/controllerUsuarios.js
+++ src/controllers/controllerUsuarios.js
@@ -6,166 +6,166 @@ const controllerUsuarios =
 {
     login: (req, res) =>
     {
-	res.render("login");
+        res.render("login");
     },
 
     loginPOST:(req,res)=>
-    {	    
-	(async () => {
-	    let errors = {};
+    {       
+        (async () => {
+            let errors = {};
 
-	    if (req.body.emailusuario == "")
-		errors.emailusuario = "Debe ingresar un email registrado";
+            if (req.body.emailusuario == "")
+                errors.emailusuario = "Debe ingresar un email registrado";
 
-	    if (req.body.contrasenausuario == "")
-		errors.contrasenausuario = "Debe ingresar una contraseña";
+            if (req.body.contrasenausuario == "")
+                errors.contrasenausuario = "Debe ingresar una contraseña";
 
-	    if (Object.keys(errors).length == 0)
-	    {
-		const usertologin = await db.user.findOne({
-		    where: {email: req.body.emailusuario}});
-	    
-		if (usertologin !== null)
-		{
-		    if (!bcrypt.compareSync(req.body.contrasenausuario, usertologin.password))
-		    {
-			req.body.contrasenausuario = null;
-			return res.render("login", {errors: {contrasenausuario: "Contraseña incorrecta"}, old: req.body});
-		    }
-		    else
-		    {
-			req.session.user = usertologin;
-			
-			res.cookie("email", usertologin.email,{maxAge:1000*60}*30)
-			
-			return res.redirect("profile");
-		    }
-		}
-		else
-		    return res.render("login", {errors: {emailusuario: "Este E-mail nunca ha sido registrado"}, old: req.body});
-	    }
-	    else
-		return res.render("login", {errors: errors, old: req.body});
-	})();
+            if (Object.keys(errors).length == 0)
+            {
+                const usertologin = await db.user.findOne({
+                    where: {email: req.body.emailusuario}});
+            
+                if (usertologin !== null)
+                {
+                    if (!bcrypt.compareSync(req.body.contrasenausuario, usertologin.password))
+                    {
+                        req.body.contrasenausuario = null;
+                        return res.render("login", {errors: {contrasenausuario: "Contraseña incorrecta"}, old: req.body});
+                    }
+                    else
+                    {
+                        req.session.user = usertologin;
+                        
+                        res.cookie("email", usertologin.email,{maxAge:1000*60}*30)
+                        
+                        return res.redirect("profile");
+                    }
+                }
+                else
+                    return res.render("login", {errors: {emailusuario: "Este E-mail nunca ha sido registrado"}, old: req.body});
+            }
+            else
+                return res.render("login", {errors: errors, old: req.body});
+        })();
     },
     
     register: (req, res) =>
     {
-	res.render("register");
+        res.render("register");
     },
 
     registerPOST: (req, res) =>
     {
-	let errors = {};
-	console.log(req.body);
-	if (req.body.nombreusuario.trim() == "")
-	    errors.nombreusuario = "Debe ingresar un nombre de usuario";
+        let errors = {};
+        console.log(req.body);
+        if (req.body.nombreusuario.trim() == "")
+            errors.nombreusuario = "Debe ingresar un nombre de usuario";
 
-	if (req.body.nombreusuario.trim().length < 8)
-	    errors.nombreusuario = "El nombre de usuario debe tener al menos 8 caracteres";
+        if (req.body.nombreusuario.trim().length < 8)
+            errors.nombreusuario = "El nombre de usuario debe tener al menos 8 caracteres";
 
-	if (req.body.nombreusuario.trim().length > 32)
-	    errors.nombreusuario = "El nombre de usuario debe respetar el maximo de 32 caracteres";
+        if (req.body.nombreusuario.trim().length > 32)
+            errors.nombreusuario = "El nombre de usuario debe respetar el maximo de 32 caracteres";
 
-	if (req.body.emailusuario.trim() == "")
-	    errors.emailusuario = "Debe ingresar una direccion de email";
+        if (req.body.emailusuario.trim() == "")
+            errors.emailusuario = "Debe ingresar una direccion de email";
 
-	if (!req.body.emailusuario.trim().match("^.+@.+$"))
-	    errors.emailusuario = "Debe ingresar una direccion de email valida";
+        if (!req.body.emailusuario.trim().match("^.+@.+$"))
+            errors.emailusuario = "Debe ingresar una direccion de email valida";
 
-	if (req.body.contrasenausuario.trim() == "")
-	    errors.contrasenausuario = "Debe ingresar una constraseña";
-	
-	if (req.body.contrasenausuario.trim().length < 8)
-	    errors.contrasenausuario = "La contraseña debe tener al menos 8 caracteres";
+        if (req.body.contrasenausuario.trim() == "")
+            errors.contrasenausuario = "Debe ingresar una constraseña";
+        
+        if (req.body.contrasenausuario.trim().length < 8)
+            errors.contrasenausuario = "La contraseña debe tener al menos 8 caracteres";
 
-	if (req.body.contrasenausuario.trim().length > 32)
-	    errors.contrasenausuario = "La contraseña debe respetar el maximo de 32 caracteres";
-	
-	if (req.body.contrasenausuario.trim() !== req.body.contrasenausuario2.trim())
-	{
-	    req.body.contrasenausuario2 = null;
-	    errors.contrasenausuario2 = "Las contraseñas no coinciden";
-	}
-	
-	if (Object.keys(errors).length == 0)
-	{
-	    (async () => {
-		let u = await db.user.findOne({where: {username: req.body.nombreusuario.trim()}});
-		if (u === null)
-		{
-		    let archivo_imagen = null;
-	
-		    if (req.file)		
-			archivo_imagen = req.file.filename;
+        if (req.body.contrasenausuario.trim().length > 32)
+            errors.contrasenausuario = "La contraseña debe respetar el maximo de 32 caracteres";
+        
+        if (req.body.contrasenausuario.trim() !== req.body.contrasenausuario2.trim())
+        {
+            req.body.contrasenausuario2 = null;
+            errors.contrasenausuario2 = "Las contraseñas no coinciden";
+        }
+        
+        if (Object.keys(errors).length == 0)
+        {
+            (async () => {
+                let u = await db.user.findOne({where: {username: req.body.nombreusuario.trim()}});
+                if (u === null)
+                {
+                    let archivo_imagen = null;
+        
+                    if (req.file)               
+                        archivo_imagen = req.file.filename;
 
-		    const [new_user, created] = await db.user.findOrCreate({
-			where: {email: req.body.emailusuario.trim()},
-			defaults: {
-			    username: req.body.nombreusuario.trim(),
-			    password: bcrypt.hashSync(req.body.contrasenausuario.trim(), 10),
-			    img: archivo_imagen
-			}});
-		    
-		    if (created)
-		    {
-			req.session.user = new_user;
-			
-			res.cookie("email", new_user.email,{maxAge:1000*60}*30)
-			
-			return res.redirect("profile");
-		    }
-		    else
-			return res.render("register", {errors: {emailusuario:  "Este E-mail ya esta registrado"}, old: req.body});
-		}
-		else
-		    return res.render("register", {errors: {nombreusuario: "Este nombre de usuario ya se encuentra en uso"}, old: req.body});
-	    })();
-	}
-	else
-	    return res.render("register", {errors: errors, old: req.body});
+                    const [new_user, created] = await db.user.findOrCreate({
+                        where: {email: req.body.emailusuario.trim()},
+                        defaults: {
+                            username: req.body.nombreusuario.trim(),
+                            password: bcrypt.hashSync(req.body.contrasenausuario.trim(), 10),
+                            img: archivo_imagen
+                        }});
+                    
+                    if (created)
+                    {
+                        req.session.user = new_user;
+                        
+                        res.cookie("email", new_user.email,{maxAge:1000*60}*30)
+                        
+                        return res.redirect("profile");
+                    }
+                    else
+                        return res.render("register", {errors: {emailusuario:  "Este E-mail ya esta registrado"}, old: req.body});
+                }
+                else
+                    return res.render("register", {errors: {nombreusuario: "Este nombre de usuario ya se encuentra en uso"}, old: req.body});
+            })();
+        }
+        else
+            return res.render("register", {errors: errors, old: req.body});
     },
     
     profile: (req, res) =>
     {
-	return res.render("profile",{userData:req.session.user});
+        return res.render("profile",{userData:req.session.user});
     },
     
     logout: (req,res) =>
     {
-	req.session.destroy();
-	res.clearCookie("email");
-	return res.redirect("/")	
+        req.session.destroy();
+        res.clearCookie("email");
+        return res.redirect("/")        
     },
 
     api: (req, res) =>
     {
-	(async () =>
-	 {
-	     let userlist = [];
-	     const {count, rows} = await db.user.findAndCountAll();
-	     rows.forEach(user =>
-			  {
-			      var tempuser = user.toJSON();
-			      delete tempuser.password;
-			      userlist.push(tempuser);
-			  });
-	     res.send({count: count, users: userlist});
-	 })();
+        (async () =>
+         {
+             let userlist = [];
+             const {count, rows} = await db.user.findAndCountAll();
+             rows.forEach(user =>
+                          {
+                              var tempuser = user.toJSON();
+                              delete tempuser.password;
+                              userlist.push(tempuser);
+                          });
+             res.send({count: count, users: userlist});
+         })();
     },
 
     apiID: (req, res) =>
     {
-	(async () => {
-	    let u = await db.user.findByPk(req.params.id);
-	    let c = await db.compra.findAll({where: {userId: req.params.id}});
-	    let compras = [];
-	    c.forEach(compra =>
-			 {
-			     compras.push(compra.toJSON());
-			 });
-	    res.send({user: u.toJSON(), compras: compras});
-	})();
+        (async () => {
+            let u = await db.user.findByPk(req.params.id);
+            let c = await db.compra.findAll({where: {userId: req.params.id}});
+            let compras = [];
+            c.forEach(compra =>
+                         {
+                             compras.push(compra.toJSON());
+                         });
+            res.send({user: u.toJSON(), compras: compras});
+        })();
     }
 };
 
